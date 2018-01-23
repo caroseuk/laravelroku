3efa8945d8b81d229da083772d364449a04b25f0
Chris Rose <chris@crose.co.uk>
2018-01-23 16:33:15 +0000

    Added ServerPilot

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel / Heroku / Codeship
+                    Laravel / Codeship -> ServerPilot
                 </div>
 
             </div>
